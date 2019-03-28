a194d3e8f3326189c19ae48bda8d9c6c13d6836e
Bryan Hadro <bryan.e.hadro@gmail.com>
2019-03-28 12:02:54 -0400

    add comments to base settings file

--- settings_base.py
+++ settings_base.py
@@ -1,3 +1,5 @@
-EMAIL = ''
-PASSWORD = ''
-MIN_MARKET_CAP = None
+# REMINDER: don't add your actual EMAIL and PASSWORD here, add them to the settings_local.py file!
+
+EMAIL = ''              # e-mail used for logging into MagicFormulaInvesting.com, e.g. 'john.smith@gmail.com'
+PASSWORD = ''           # password used for logging into MagicFormulaInvesting.com, e.g. 'changeme123!'
+MIN_MARKET_CAP = None   # e.g. 41
